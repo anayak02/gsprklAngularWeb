1600d435f1e9269c2eb8b01fce24cfdf96c91f1b
Arjun Nayak <arjun430@gmail.com>
2021-03-09 21:38:02 +0530

    change layout

--- src/app/theme/layout/admin/navigation/navigation.ts
+++ src/app/theme/layout/admin/navigation/navigation.ts
@@ -28,7 +28,7 @@ export interface Navigation extends NavigationItem {
 const NavigationItems = [
   {
     id: 'navigation',
-    title: 'Navigation',
+    title: 'Menu',
     type: 'group',
     icon: 'feather icon-monitor',
     children: [
@@ -64,166 +64,7 @@ const NavigationItems = [
       }
     ]
   },
-  {
-    id: 'ui-element',
-    title: 'UI ELEMENT',
-    type: 'group',
-    icon: 'feather icon-layers',
-    children: [
-      {
-        id: 'basic',
-        title: 'Basic',
-        type: 'collapse',
-        icon: 'feather icon-box',
-        children: [
-          {
-            id: 'alert',
-            title: 'Alert',
-            type: 'item',
-            url: '/basic/alert'
-          },
-          {
-            id: 'button',
-            title: 'Button',
-            type: 'item',
-            url: '/basic/button'
-          },
-          {
-            id: 'badges',
-            title: 'Badges',
-            type: 'item',
-            url: '/basic/badges'
-          },
-          {
-            id: 'breadcrumb-pagination',
-            title: 'Breadcrumbs & Pagination',
-            type: 'item',
-            url: '/basic/breadcrumb-paging'
-          },
-          {
-            id: 'cards',
-            title: 'Cards',
-            type: 'item',
-            url: '/basic/cards'
-          },
-          {
-            id: 'collapse',
-            title: 'Collapse',
-            type: 'item',
-            url: '/basic/collapse'
-          },
-          {
-            id: 'carousel',
-            title: 'Carousel',
-            type: 'item',
-            url: '/basic/carousel'
-          },
-          {
-            id: 'grid-system',
-            title: 'Grid System',
-            type: 'item',
-            url: '/basic/grid-system'
-          },
-          {
-            id: 'progress',
-            title: 'Progress',
-            type: 'item',
-            url: '/basic/progress'
-          },
-          {
-            id: 'modal',
-            title: 'Modal',
-            type: 'item',
-            url: '/basic/modal'
-          },
-          {
-            id: 'spinner',
-            title: 'Spinner',
-            type: 'item',
-            url: '/basic/spinner'
-          },
-          {
-            id: 'tabs-pills',
-            title: 'Tabs & Pills',
-            type: 'item',
-            url: '/basic/tabs-pills'
-          },
-          {
-            id: 'typography',
-            title: 'Typography',
-            type: 'item',
-            url: '/basic/typography'
-          },
-          {
-            id: 'tooltip-popovers',
-            title: 'Tooltip & Popovers',
-            type: 'item',
-            url: '/basic/tooltip-popovers'
-          },
-          {
-            id: 'toasts',
-            title: 'Toasts',
-            type: 'item',
-            url: '/basic/toasts'
-          },
-          {
-            id: 'other',
-            title: 'Other',
-            type: 'item',
-            url: '/basic/other'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    id: 'forms',
-    title: 'Forms & Table',
-    type: 'group',
-    icon: 'feather icon-layout',
-    children: [
-      {
-        id: 'forms-element',
-        title: 'Forms',
-        type: 'item',
-        url: '/forms/basic',
-        classes: 'nav-item',
-        icon: 'feather icon-file-text'
-      },
-      {
-        id: 'bootstrap',
-        title: 'Bootstrap',
-        type: 'item',
-        url: '/tbl-bootstrap/bt-basic',
-        classes: 'nav-item',
-        icon: 'feather icon-server'
-      }
-    ]
-  },
-  {
-    id: 'chart-maps',
-    title: 'Chart & Maps',
-    type: 'group',
-    icon: 'feather icon-pie-chart',
-    children: [
-      {
-        id: 'charts',
-        title: 'Charts',
-        type: 'item',
-        url: '/charts/apex',
-        classes: 'nav-item',
-        icon: 'feather icon-pie-chart'
-      },
-      {
-        id: 'maps',
-        title: 'Maps',
-        type: 'item',
-        url: '/maps/google',
-        classes: 'nav-item',
-        icon: 'feather icon-map'
-      }
-    ]
-  },
+  
   {
     id: 'pages',
     title: 'Pages',
@@ -253,14 +94,6 @@ const NavigationItems = [
             breadcrumbs: false
           }
         ]
-      },
-      {
-        id: 'sample-page',
-        title: 'Sample Page',
-        type: 'item',
-        url: '/sample-page',
-        classes: 'nav-item',
-        icon: 'feather icon-sidebar'
       }
     ]
   }
